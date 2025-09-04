be944792de1c29ef22bf2e29c6324c0b2116f3db
Sriajay S <sriajaysanipini047@gmail.com>
2025-09-04 21:56:54 +0530

    added home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+
+function Home() {
+  return (
+    <div>
+        <Header/>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
