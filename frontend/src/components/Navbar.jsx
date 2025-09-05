59ed54caa0b39b535830411ca534f5b1a67a72c9
Sriajay S <sriajaysanipini047@gmail.com>
2025-09-05 22:42:28 +0530

    added seach favicon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav>
       <div className="bg-[#e40046] flex items-center justify-around px-6 py-3">
-     
+
         <div className="flex items-center space-x-2">
           <Link to="/">
             <img
@@ -19,27 +19,35 @@ function Navbar() {
         </div>
 
         <div className="flex bg-white w-[45%]">
+          
           <input
             type="text"
             placeholder="Search product & brands"
             className="flex-grow px-6 py-2 outline-none text-sm"
           />
+          
           <button className="bg-[#333333] text-white px-6 text-sm font-medium hover:bg-black cursor-pointer">
+            <i className="fa-solid fa-search text-white-600 text-md pt-2 pr-1"></i>
             Search
           </button>
+         
         </div>
 
-       
+
         <div className="flex items-center gap-2 text-[13px] cursor-pointer">
           <span className="text-white">Cart</span>
           <img src={cartlogo} alt="Cart" className="h-5 w-6" />
         </div>
 
-
+          
         <div className="flex items-center gap-5 text-[13px] cursor-pointer">
           <span className="text-white">Sign In</span>
           <img src={signin} alt="Sign In" className="h-5 w-6" />
         </div>
+     
+
+
+
       </div>
 
     </nav>
