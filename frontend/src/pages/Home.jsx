c0ad16dea04b3bf5bd47deeec27c12750628773b
Sriajay S <sriajaysanipini047@gmail.com>
2025-09-05 22:44:19 +0530

    created  sidebar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import Carousel from '../components/Carousel'
 
-function Home() {
-  return (
-    <div>
-        <Header/>
-        <Navbar/>
-       
-    </div>
-  )
+function Home() { 
+  
+  
+  return ( <div> 
+    <Header/> 
+    <Navbar/>
+     <div className='bg-[#f7f7f7] pt-4 flex gap-4'> 
+      <Sidebar/> 
+     
+      </div> 
+       </div> 
+      ) 
+
 }
 
-export default Home
+export default Home;
+
+
