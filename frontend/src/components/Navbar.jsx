75a00338ba892080c7a2e1f7b43f3772fae6cd82
Sriajay S <sriajaysanipini047@gmail.com>
2025-09-05 13:27:50 +0530

    added tooltip and cursor pointer

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,43 +5,43 @@ import signin from "../assets/signin.png";
 
 function Navbar() {
   return (
-    <nav className="w-full shadow-md">
+    <nav>
       <div className="bg-[#e40046] flex items-center justify-around px-6 py-3">
-        {/* Logo */}
+     
         <div className="flex items-center space-x-2">
           <Link to="/">
             <img
               src="https://i3.sdlcdn.com/img/snapdeal/darwin/logo/sdLatestLogo.svg"
-              alt="Snapdeal"
+              alt="Snapdeal" title="Snapdeal"
               className="h-8"
             />
           </Link>
         </div>
 
-        {/* Search Bar */}
-        <div className="flex bg-white rounded overflow-hidden w-1/2">
+        <div className="flex bg-white w-[45%]">
           <input
             type="text"
             placeholder="Search product & brands"
-            className="flex-grow px-4 py-2 outline-none text-sm"
+            className="flex-grow px-6 py-2 outline-none text-sm"
           />
-          <button className="bg-[#333333] text-white px-6 text-sm font-medium hover:bg-black">
+          <button className="bg-[#333333] text-white px-6 text-sm font-medium hover:bg-black cursor-pointer">
             Search
           </button>
         </div>
 
-        {/* Cart */}
+       
         <div className="flex items-center gap-2 text-[13px] cursor-pointer">
           <span className="text-white">Cart</span>
           <img src={cartlogo} alt="Cart" className="h-5 w-6" />
         </div>
 
-        {/* Sign In */}
-        <div className="flex items-center gap-2 text-[13px] cursor-pointer">
+
+        <div className="flex items-center gap-5 text-[13px] cursor-pointer">
           <span className="text-white">Sign In</span>
           <img src={signin} alt="Sign In" className="h-5 w-6" />
         </div>
       </div>
+
     </nav>
   );
 }
